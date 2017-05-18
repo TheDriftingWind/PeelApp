a09d18a83f165cbf345c1a301464bf883cba3c8c
Charles Zhu <CharlesYZhu@yahoo.com>
2017-05-18 15:22:23 -0400

    Initial commit

---
+++ desktop/src/com/peel/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.peel.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.peel.game.PeelGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new PeelGame(), config);
+	}
+}
