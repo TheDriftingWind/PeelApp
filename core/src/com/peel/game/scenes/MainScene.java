08f8ccbbff882991c92a1123e2748c3151e6d008
Charles Zhu <CharlesYZhu@yahoo.com>
2017-05-19 15:30:28 -0400

    listeners added, unimplemented

--- core/src/com/peel/game/scenes/MainScene.java
+++ core/src/com/peel/game/scenes/MainScene.java
@@ -6,12 +6,14 @@ import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.peel.game.PeelGame;
@@ -41,42 +43,51 @@ public class MainScene {
     private Skin skin;
 
     public MainScene(SpriteBatch sb){
-        //Setup
+        //Setup-----------------------------------------------------------------
         viewport = new FitViewport(PeelGame.V_WIDTH, PeelGame.V_HEIGHT, new OrthographicCamera());
         stage = new Stage(viewport, sb);
         font = new BitmapFont();
         skin = new Skin();
-        //Table
+        //Table-----------------------------------------------------------------
         Table table = new Table();
         table.setFillParent(true);
         table.center();
-
-        //Label
+        //Label-----------------------------------------------------------------
         titleLabel = new Label(PeelGame.TITLE, new Label.LabelStyle(font, Color.WHITE));
         titleLabel.setFontScale(10,7);
-
-        //Buttons Setup
+        //Buttons Setup-----------------------------------------------------------------
         buttonAtlas = new TextureAtlas("ui/button.pack");
         textButtonStyle = new TextButton.TextButtonStyle();
         skin.addRegions(buttonAtlas);
         textButtonStyle.font = font;
         textButtonStyle.up = skin.getDrawable("button.up");
         textButtonStyle.down = skin.getDrawable("button.down");
-
-        //Buttons
+        //Buttons-----------------------------------------------------------------
         playButton = new TextButton("PLAY", textButtonStyle);
         playButton.getLabel().setFontScale(7,5);
         settingsButton = new TextButton("SETTINGS", textButtonStyle);
         settingsButton.getLabel().setFontScale(7,5);
-
-        //table
+        //Button Listeners------------------------------------------------------------------
+        playButton.addListener(new ClickListener(){
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                super.clicked(event, x, y);
+            }
+        });
+        settingsButton.addListener(new ClickListener(){
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                super.clicked(event, x, y);
+            }
+        });
+        //TABLE-----------------------------------------------------------------
         table.add(titleLabel).expand().padTop(10);
         table.row();
         table.add(playButton).expand().padTop(10);
         table.row();
         table.add(settingsButton).expand().padTop(10);
 
-        //Stage
+        //Stage-----------------------------------------------------------------
         stage.addActor(table);
     }
 
